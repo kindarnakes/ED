d6d8e4b29f43b45ad4c423840f5a60228ebe951d
kindarnakes <kindenene@gmail.com>
2020-02-14 14:28:14 +0100

    Añadido año

--- src/model/Fecha.java
+++ src/model/Fecha.java
@@ -6,4 +6,16 @@ package model;
  */
 public class Fecha {
     
+    private int _anno;
+
+    public int getAnno() {
+        return _anno;
+    }
+
+    public void setAnno(int _anno) {
+        this._anno = _anno;
+    }
+    
+    
+    
 }
