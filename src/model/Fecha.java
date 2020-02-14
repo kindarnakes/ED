da287ff48c16daac3f9a29ba3c21bf1eed172a38
Starlok23 <pacomartin2230@gmail.com>
2020-02-14 14:28:21 +0100

    fecha

--- src/model/Fecha.java
+++ src/model/Fecha.java
@@ -5,5 +5,23 @@ package model;
  * @author guerr
  */
 public class Fecha {
-    
+ private int _dia;
+ private int _mes;
+
+    public int getDia() {
+        return _dia;
+    }
+
+    public void setDia(int _dia) {
+        this._dia = _dia;
+    }
+
+    public int getMes() {
+        return _mes;
+    }
+
+    public void setMes(int _mes) {
+        this._mes = _mes;
+    }
+ 
 }
